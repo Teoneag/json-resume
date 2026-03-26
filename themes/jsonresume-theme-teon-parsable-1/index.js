3dced3fa55190bd5b272ab8a5ed6f405aca052bd
Teoneag <teodor.neagoe.climber@gmail.com>
2026-03-26 15:26:27 +0100

    feat: custom theme: teon-parsable-1

---
+++ themes/jsonresume-theme-teon-parsable-1/index.js
@@ -0,0 +1,27 @@
+var fs = require('fs');
+var Handlebars = require('handlebars');
+
+function render(resume) {
+  // Split education courses into 3 columns for layout
+  (resume.education || []).forEach(function(block) {
+    if (Array.isArray(block.courses) && block.courses.length) {
+      var cols = [[], [], []];
+      block.courses.forEach(function(c, i) { cols[i % 3].push(c); });
+      block.courses = cols;
+    }
+  });
+
+  // Ensure highlights array exists (guards {{#if highlights.length}} in template)
+  ['work', 'volunteer', 'awards', 'publications'].forEach(function(key) {
+    (resume[key] || []).forEach(function(block) {
+      block.highlights = block.highlights || [];
+    });
+  });
+
+  return Handlebars.compile(fs.readFileSync(__dirname + '/resume.hbs', 'utf-8'))({
+    css: fs.readFileSync(__dirname + '/style.css', 'utf-8'),
+    resume: resume
+  });
+}
+
+module.exports = { render: render };
